846f10c599b2469012818af7d928afe89f1947fa
seoyumin1018 <yumin1018@kakao.com>
2023-11-20 15:47:38 +0900

    장바구니 수량 총금액

--- BBMR_Project/app/src/main/java/com/example/bbmr_project/Dialog/Normal_SelectPayDialog.kt
+++ BBMR_Project/app/src/main/java/com/example/bbmr_project/Dialog/Normal_SelectPayDialog.kt
@@ -14,7 +14,11 @@ import com.example.bbmr_project.R
 import com.example.bbmr_project.databinding.DialogNormalSelectPayBinding
 
 interface NormalSelectPayDialogListener : Normal_MenuDialogListener {
-    override fun onMenuAdded(normalSelectedMenuInfo: NormalSelectedMenuInfo, tvCount: Int)
+    override fun onMenuAdded(
+        normalSelectedMenuInfo: NormalSelectedMenuInfo,
+        tvCount: Int,
+        totalCost: Int
+    )
 }
 
 class Normal_SelectPayDialog : DialogFragment() {
